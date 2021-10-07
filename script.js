dc2b6d42a38b4a2b4d930bb2c37be1703a08a77c
lizzyjane340 <lizzyjane0121@protonmail.com>
2021-10-07 20:56:27 +0100

    Create external JS file

---
+++ script.js
@@ -0,0 +1,29 @@
+/* functions to change button colors */
+        function myHover(elem) {
+            elem.style.backgroundColor = "#CBC5C8";
+        }
+
+        function myLeave(elem) {
+            elem.style.backgroundColor = "#FFFFFF";
+        }
+
+// input length validation for forms
+const emailInput = document.getElementById('emailInput');
+
+if (emailInput != null) {  
+  emailInput.addEventListener('input', () => {
+  if (!emailInput.checkValidity()) {
+    emailInput.innerHTML = emailInput.validationMessage;
+    } 
+  });
+}
+
+const messageInput = document.getElementById('messageInput');
+
+if (messageInput != null) {  
+    messageInput.addEventListener('input', () => {
+    if (!messageInput.checkValidity()) {
+      messageInput.innerHTML = messageInput.validationMessage;
+      } 
+    });
+  }
