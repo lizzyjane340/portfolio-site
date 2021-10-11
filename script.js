17d296214b776435da50e7c7afd78beb0fdc5bc5
lizzyjane340 <lizzyjane0121@protonmail.com>
2021-10-11 01:58:34 +0100

    Creating a new link bar href link

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 // input length validation for forms
 const emailInput = document.getElementById('emailInput');
 
-if (emailInput != null) {  
+if (emailInput != null) {
   emailInput.addEventListener('input', () => {
   if (!emailInput.checkValidity()) {
     emailInput.innerHTML = emailInput.validationMessage;
-    } 
+    }
   });
 }
 
 const messageInput = document.getElementById('messageInput');
 
-if (messageInput != null) {  
+if (messageInput != null) {
     messageInput.addEventListener('input', () => {
     if (!messageInput.checkValidity()) {
       messageInput.innerHTML = messageInput.validationMessage;
-      } 
+      }
     });
   }
